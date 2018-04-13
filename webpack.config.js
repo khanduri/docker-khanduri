53862edd03217bdb1f6d355dd4ba6bd3b8ba7152
Prashant Khanduri <prashant.khanduri@gmail.com>
2018-04-13 16:19:32 +0530

    moving remote google font links into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,14 @@ module.exports = {
             use: ['url-loader']
          },
          {
-            test: /\.(svg|woff|woff2|ttf|eot)(\?.*$|$)/,
-            use: ['file-loader']
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+               loader: 'file-loader',
+               options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+               }
+            }]
          }
       ]
    },
