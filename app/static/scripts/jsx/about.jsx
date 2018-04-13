b678a7a42fea0348ff294ceb6c4eff0e5000e8fa
Prashant Khanduri <prashant.khanduri@gmail.com>
2018-04-13 18:53:24 +0530

    about section moved for prashant

--- app/static/scripts/jsx/about.jsx
+++ app/static/scripts/jsx/about.jsx
@@ -1,19 +1,26 @@
 import ReactDOM from 'react-dom';
 
-/* About */
-function AboutPrashantComponent() {
+
+function AboutComponentItem(props){
+  var aboutItem = props.items;
+  return (<div className="about-item" dangerouslySetInnerHTML={{__html: aboutItem}}></div>)
+}
+
+
+function AboutComponent(props) {
+    var about = props.items;
+    
+    var rows = [];
+    about.map(function(aboutItem, i){
+      rows.push(<AboutComponentItem items={aboutItem} key={'about_item_' + i}/>);
+    });
+
     return (
         <section className="content-section text-center about">
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2">
               <h2>About Me</h2>
-              <p>Hi there! I'm Prashant Khanduri</p>
-              <p>I'm originally from India. Although most of my school days were spent in India (New Delhi), a few of them slipped into Nepal (Kathmandu), Canada (Waterloo) and United States (Los Angeles)</p>
-              <p>On a day to day basis, I tell computers what to do and they seem to obey me (for the most part). I end up getting paid for <a href='#' ng-click="scroll('#experience')">some of it</a>, the other portion ends up on a <a href="https://github.com/khanduri/">public domain</a></p>
-              <p>I also <a href="http://khanduri.github.io/">blog</a>, but I haven't really scoped out the topics I'd like to talk about. For now, I'm just using it to collect ideas that I find interesting. I hope someday a pattern will emerge</p>
-              <p></p>
-              <p>In case you're wondering, the above picture is me trying to slide down Mt Rainier (Paradise snowplay area) and not doing a great job at it. I like the picture though</p>
-              <p>You know a lot about me now! .. <a href='#contact' className="page-scroll">Ok your turn</a></p>
+              {rows}
             </div>
           </div>
         </section>
@@ -46,10 +53,6 @@ function AboutPuneetComponent(){
     )
 }
 
-if (info.about == 'prashant'){
-  ReactDOM.render(<AboutPrashantComponent />, document.getElementById('about'));
-}
-
-if (info.about == 'puneet'){
-  ReactDOM.render(<AboutPuneetComponent />, document.getElementById('about'));
+if (info.about){
+  ReactDOM.render(<AboutComponent items={info.about} />, document.getElementById('about'));
 }
